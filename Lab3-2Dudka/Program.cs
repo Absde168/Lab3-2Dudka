1513b8e1fc8f5fcee41a46db5b0e2c7ffdb646c4
B3dRandoms <bdrandoms0@gmail.com>
2023-12-27 23:23:21 +0200

    Initial commit

---
+++ Lab3-2Dudka/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите числа, разделяя их запятой (для завершения введите отрицательное число):");
+string input = Console.ReadLine();
+string[] numbers = input.Split(',');
+
+int[] sequence = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{
+    sequence[i] = int.Parse(numbers[i]);
+}
+
+int firstNegativeIndex = Array.IndexOf(sequence, Array.Find(sequence, x => x < 0));
+
+for (int i = 0; i < firstNegativeIndex; i++)
+{
+    if (sequence[i] >= 3 && sequence[i] <= 13)
+    {
+        Console.WriteLine(sequence[i]);
+    }
+}
